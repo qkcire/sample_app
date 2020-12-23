a467c98180602c0355d6b98e30c9bc757ac647c1
qkcire <equintanilla@berkeley.edu>
2020-12-23 10:37:16 -0800

    Add a full-title helper

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+  def home 
+  end
+
+  def help 
+  end
+
+  def about 
+  end
+
+  def contact
+  end
+end
+
+
+def hello
+  if 2 < 3
+    puts "Hi"
+  elsif 3 < 4
+    puts "Hello"
+
+  end
+end
